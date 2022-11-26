6fee4152513c0a9b0cca50009d2db2372e8eaaa9
siddhantjain556 <118827813+siddhantjain556@users.noreply.github.com>
2022-11-26 21:40:36 +0530

    Add files via upload

---
+++ sum_of_2_numbers.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main (){
+    int a,b;
+    //This is a comment
+    /*This is A multiline comment
+    hello*/
+    cin>>a>>b;
+    cout<<"The Sum of 2 Numbers are : "<<a+b;
+    return 0;
+
+}
